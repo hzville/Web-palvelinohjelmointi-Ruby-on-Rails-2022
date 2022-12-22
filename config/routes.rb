f9de2c01a9934e65538c3e00a2fa8685ca9fd00f
Ville Halonen <71012693+hzville@users.noreply.github.com>
2022-12-22 13:27:11 +0200

    add navbar, ratings model

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  root 'breweries#index'
   resources :beers
   resources :breweries
+  get 'ratings', to: 'ratings#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
