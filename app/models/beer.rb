bff0f24f3571cf99e5faa4226f62ce36270f3fa9
Ville Halonen <71012693+hzville@users.noreply.github.com>
2022-12-22 16:47:32 +0200

    wk2. ex.4

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,4 +1,13 @@
 class Beer < ApplicationRecord
     belongs_to :brewery
     has_many :ratings
+
+    def average_rating
+        sum = 0
+        for rating in ratings do
+            sum += rating.score
+        end
+        float_number = sum/ratings.count.to_f.truncate(1)
+        float_number.truncate(1)
+    end
 end
