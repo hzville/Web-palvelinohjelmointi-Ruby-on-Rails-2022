b628b5085a2f5db28373f521adac15c1a46d3235
Ville Halonen <71012693+hzville@users.noreply.github.com>
2022-12-22 23:12:28 +0200

    wk.2 ex.15

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,12 +1,8 @@
 class Beer < ApplicationRecord
+include RatingAverage 
+
     belongs_to :brewery
     has_many :ratings, dependent: :destroy
-
-    def average_rating
-        result = ratings.reduce(0){|sum, rating| sum+rating.score}
-        float_number = result/ratings.count.to_f.truncate(1)
-        float_number.truncate(1)
-    end
 
     def to_s
         name + ", " + brewery.name
--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -1,15 +1,11 @@
 class Brewery < ApplicationRecord
+include RatingAverage
+
     has_many :beers, dependent: :destroy
     has_many :ratings, through: :beers
 
     def to_s
         name
-    end
-
-    def average_rating
-        result = ratings.reduce(0){|sum, rating| sum+rating.score}
-        float_number = result/ratings.count.to_f.truncate(1)
-        float_number.truncate(1)
     end
     
 end
