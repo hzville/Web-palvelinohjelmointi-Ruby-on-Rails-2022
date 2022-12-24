3cb361306f48b5b66d168dfb4137ad5f4563ecf8
Ville Halonen <71012693+hzville@users.noreply.github.com>
2022-12-24 21:03:58 +0200

    wk.3 ex.12

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
 class User < ApplicationRecord
   include RatingAverage
+  has_secure_password
   validates :username, uniqueness: true,
                        length: { minimum: 3, maximum: 30 }
+  validates :password, length: { minimum: 4}, format: {with: /(?=.*[A-Z])(?=.*[0-9]).*\z/}  
   has_many :ratings
   has_many :beers, through: :ratings
 end
