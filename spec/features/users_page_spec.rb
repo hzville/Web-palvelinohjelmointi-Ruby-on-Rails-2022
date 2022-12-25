34de71d642237395f9917d8563fafdbbc3dd0432
Ville Halonen <71012693+hzville@users.noreply.github.com>
2022-12-25 21:28:48 +0200

    wk.4 ex.8

--- spec/features/users_page_spec.rb
+++ spec/features/users_page_spec.rb
@@ -13,12 +13,14 @@ describe "User" do
       expect(page).to have_content 'Welcome back!'
       expect(page).to have_content 'Pekka'
     end
+
     it "is redirected back to signin form if wrong credentials given" do
         sign_in(username: "Pekka", password: "wrong")
         expect(current_path).to eq(signin_path)
         expect(page).to have_content 'Username and/or password mismatch'
     end
   end
+
   it "when signed up with good credentials, is added to the system" do
     visit signup_path
     fill_in('user_username', with: 'Brian')
@@ -29,6 +31,7 @@ describe "User" do
       click_button('Create User')
     }.to change{User.count}.by(1)
   end
+
   it 'User ratings are visible on users own page' do
     user1 = FactoryBot.create :user, username: 'test_user1', password: 'Test12', password_confirmation: 'Test12'
     user2 = FactoryBot.create :user, username: 'test_user2', password: 'Test23', password_confirmation: 'Test23'
@@ -45,6 +48,15 @@ describe "User" do
     expect(page).to have_content 'anonymous 15 test_user1'
     expect(page).to have_content 'anonymous 25 test_user1'
     expect(page).to have_content 'anonymous 33 test_user2'
+  end
+
+  it 'User rating is removed from db when pressed delete' do
+    user1 = FactoryBot.create :user, username: 'test_user1', password: 'Test12', password_confirmation: 'Test12'
+    FactoryBot.create :rating, score: 15, user: user1
+    sign_in(username: "test_user1", password: "Test12")
+    expect(Rating.count).to eq(1)
+    click_link 'Delete'
+    expect(Rating.count).to eq(0)
 
   end
 end
